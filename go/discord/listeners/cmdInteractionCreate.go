1ae45ed189a0b540aafca9f734fa866f4dcc8a57
Matthew Pombo <datacorruption10@gmail.com>
2025-11-16 17:37:19 -0800

    wip listeners

--- go/discord/listeners/cmdInteractionCreate.go
+++ go/discord/listeners/cmdInteractionCreate.go
@@ -1,3 +1,60 @@
 package listeners
 
-// hewwo
+import (
+	"slices"
+	"sprout/go/app"
+	"sprout/go/discord/commands"
+	"sprout/go/platform/database/config"
+
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/events"
+)
+
+func OnCommandInteraction(a *app.App, event *events.ApplicationCommandInteractionCreate) {
+	go func() {
+		// get command
+		var cmdName string = event.Data.CommandName()
+		a.Log.Debugf("Command interaction received: %s", cmdName)
+		command, ok := commands.List[cmdName]
+		if !ok {
+			a.Log.Warnf("Unknown command: %s", cmdName)
+			return
+		}
+
+		// bot check
+		if command.FilterBots && event.User().Bot {
+			if err := event.Respond(discord.InteractionResponseTypeCreateMessage, discord.NewMessageCreateBuilder().
+				SetContent("Bots cannot use this command.").
+				SetEphemeral(true).
+				Build()); err != nil {
+				a.Log.Errorf("Error responding to interaction: %s", err)
+			}
+			return
+		}
+
+		// admin check
+		if command.RequireAdmin {
+			// get general setting
+			settings, err := config.Get[config.GeneralSettings](a.Config, "generalSettings")
+			if err != nil {
+				a.Log.Errorf("Error getting general settings from config: %s", err)
+				return
+			}
+			userID := event.User().ID.String()
+			if !slices.Contains(settings.AdminWhitelist, userID) {
+				a.Log.Warnf("User %s is not an admin", userID)
+				if err := event.Respond(discord.InteractionResponseTypeCreateMessage, discord.NewMessageCreateBuilder().
+					SetContent("You do not have permission to use this command.").
+					SetEphemeral(true).
+					Build()); err != nil {
+					a.Log.Errorf("Error responding to interaction: %s", err)
+				}
+				return
+			}
+		}
+
+		if err := command.Handler(a, event); err != nil {
+			a.Log.Errorf("Error handling command %s: %s", cmdName, err)
+		}
+	}()
+}
--- go/discord/listeners/guildMessageCreate.go
+++ go/discord/listeners/guildMessageCreate.go
@@ -1,3 +1,28 @@
 package listeners
 
-// Listener does per link, msg revive cmd new msg response to og with link to downloaded asset. If solo link attempt expand.
+import (
+	"sprout/go/app"
+
+	"github.com/disgoorg/disgo/events"
+)
+
+func OnGuildMessageCreate(a *app.App, event *events.GuildMessageCreate) {
+	go func() {
+		message, err := a.Client.Rest.GetMessage(event.Message.ChannelID, event.Message.ID)
+		if err != nil {
+			a.Log.Error("Failed to get message: ", err)
+			return
+		}
+		if message.Author.Bot {
+			return
+		}
+
+		// TODO: Listener does per link, msg revive cmd new msg response to og with link to downloaded asset. If solo link attempt expand.
+
+		/* old code
+		if err := expand.Expand(ctx, message, false); err != nil {
+			xlog.Error(ctx, "Failed to expand message: ", err)
+		}
+		*/
+	}()
+}
